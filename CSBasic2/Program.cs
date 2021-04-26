e2cbcb7b8ab9345304a6e6773ebaf5e382c7d38f
yj310 <yoenjii310@gmail.com>
2021-04-26 15:50:12 +0900

    #14 삼항연산자 예제

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -116,6 +116,31 @@ namespace CSBasic2
 
 
 
+            Console.WriteLine();
+            Console.WriteLine("자연수 판별기");
+            Console.Write("숫자를 입력하세요: ");
+            int number7 = int.Parse(Console.ReadLine());
+            Console.WriteLine((number7 > 0) ? "자연수" : "자연수 아님");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
