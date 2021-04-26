08cfa4e6265f104411ccd92fdeeda4af48ae9c29
yj310 <yoenjii310@gmail.com>
2021-04-26 15:44:06 +0900

    #15 switch 예제

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -64,6 +64,60 @@ namespace CSBasic2
 
 
 
+            Console.WriteLine();
+
+            // switch 활용
+            Console.Write("숫자를 입력하세요 : ");
+            int input3 = int.Parse(Console.ReadLine());
+            // magic number 주의
+            const int ZERO = 0;
+            const int ONE = 1;
+            switch(input3 % 2)
+            {
+                case ZERO:
+                    Console.WriteLine("짝수");
+                    break;
+                case ONE:
+                    Console.WriteLine("홀수");
+                    break;
+            }
+
+
+
+            Console.Write("이번 달은 몇 월인가요: ");
+            int input4 = int.Parse(Console.ReadLine());
+            switch(input4)
+            {
+                case 12: 
+                case 1: 
+                case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가울");
+                    break;
+                default:
+                    Console.WriteLine("지구인가요?");
+                    break;
+            }
+
+
+
+
+
+
 
 
 
