eff2b4f605758dd1d3d1a9e51c67812c0293eb5c
yj310 <yoenjii310@gmail.com>
2021-04-26 14:39:41 +0900

    #16 if 문법 및 시각 구하는 코드

--- CSBasic2/Program.cs
+++ CSBasic2/Program.cs
@@ -6,7 +6,41 @@ namespace CSBasic2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("숫자 입력: ");
+            int input = int.Parse(Console.ReadLine());
+            if(input % 2 == 0)
+            {
+                Console.WriteLine("짝수");
+            }
+            if(input % 2 == 1)
+            {
+                Console.WriteLine("홀수!");
+            }
+            Console.WriteLine();
+
+            // 시각 구하기
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+            Console.WriteLine(DateTime.Now.Millisecond);
+            Console.WriteLine();
+
+            if(DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다.");
+            }
+            if (DateTime.Now.Hour == 12)
+            {
+                Console.WriteLine("점심시간입니다.");
+            }
+            if (DateTime.Now.Hour > 12)
+            {
+                Console.WriteLine("오후입니다.");
+            }
+            Console.WriteLine("\n===\n");
         }
     }
 }
