0460f1f75a669be410fccb48783a494b484646f2
yj310 <yoenjii310@gmail.com>
2021-05-03 15:30:09 +0900

    #18 while 반복문

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -25,7 +25,42 @@ namespace CSBasic3
             int[] intArray2 = new int[1000];
             Console.WriteLine(intArray2[0]);
             Console.WriteLine(intArray2[999]);
+            Console.WriteLine();
 
+
+            int i = 0;
+            int[] intArray3 = { 52, 273, 32, 65, 103 };
+            while(i < intArray3.Length)
+            {
+                Console.WriteLine(i + "번째 출력: " + intArray3[i]);
+                i++;
+            }
+            Console.WriteLine();
+
+
+            string input;
+            do
+            {
+                Console.Write("입력(exit를 입력하면 종료): ");
+                input = Console.ReadLine();
+                switch(input)
+                {
+                    case "1":
+                        Console.WriteLine("go 공지사항");
+                        break;
+                    case "2":
+                        Console.WriteLine("go 오늘의 점심");
+                        break;
+                    default:
+                        Console.WriteLine("잘못된 입력입니다.");
+                        break;
+
+                }
+            } while (input != "exit");
+
+
+
+            Console.WriteLine();
 
         }
     }
