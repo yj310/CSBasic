be860062c0f1e7b1f1dacaecfce90ef92b6af45c
yj310 <yoenjii310@gmail.com>
2021-05-03 14:57:27 +0900

    #17 반복문과 배열 실습

--- CSBasic3/Program.cs
+++ CSBasic3/Program.cs
@@ -10,6 +10,23 @@ namespace CSBasic3
     {
         static void Main(string[] args)
         {
+
+            int[] intArray = { 52, 172, 13, 13, 103 };
+            intArray[2] = 0;
+
+            Console.WriteLine(intArray[0]);
+            Console.WriteLine(intArray[1]);
+            Console.WriteLine(intArray[2]);
+            Console.WriteLine(intArray[3]);
+            Console.WriteLine(intArray[4]);
+            Console.WriteLine(intArray.Length);
+            Console.WriteLine();
+
+            int[] intArray2 = new int[1000];
+            Console.WriteLine(intArray2[0]);
+            Console.WriteLine(intArray2[999]);
+
+
         }
     }
 }
