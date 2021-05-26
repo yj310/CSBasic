24b13f654b129aa20a6250eb51c5ab1784ddfab4
yoenjii310 <yoenjii310@gmail.com>
2021-05-26 10:20:46 +0900

    #23 추상화, 클래스 응용 실습

--- CSBasic4/Program.cs
+++ CSBasic4/Program.cs
@@ -128,6 +128,29 @@ namespace CSBasic4
 
 
 
+            Console.WriteLine();
+
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student() { name = "윤인성", grade = 1 });
+            students.Add(new Student() { name = "연하진", grade = 2 });
+            students.Add(new Student() { name = "윤아린", grade = 3 });
+            students.Add(new Student() { name = "윤명월", grade = 4 });
+            students.Add(new Student() { name = "구지연", grade = 1 });
+            students.Add(new Student() { name = "김연화", grade = 2 });
+            students.Add(new Student() { name = "함기훈", favorityFood = "밀크티" });
+
+            List<Student> students2 = new List<Student>()
+            {
+                new Student() { name = "윤인성", grade = 1 },
+                new Student() { name = "연하진", grade = 2 },
+                new Student() { name = "윤아린", grade = 3 },
+                new Student() { name = "윤명월", grade = 4 },
+                new Student() { name = "구지연", grade = 1 },
+                new Student() { name = "김연화", grade = 2 },
+                new Student() { name = "함기훈", favorityFood = "밀크티" }
+            };
+
         }
     }
 
---
+++ CSBasic4/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSBasic4
+{
+    // 모델 클래스
+    class Student
+    {
+
+        public string id;
+        public string name;
+        public int grade;
+        public string major;
+        public DateTime birthday;
+        public string favorityFood;
+
+
+    }
+}
