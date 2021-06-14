787d3ddba4a399f6261d5ab9203f93848a150dda
yoenjii310 <yoenjii310@gmail.com>
2021-06-14 14:56:52 +0900

    #26 메서드 기본, 매개변수 및 반환, 클래스 메서드 실습

--- CSBasic5/Program.cs
+++ CSBasic5/Program.cs
@@ -10,6 +10,65 @@ namespace CSBasic5
     {
         static void Main(string[] args)
         {
+
+            MultiClass m1 = new MultiClass();
+
+            Console.WriteLine(m1.Multi(52, 273));
+            Console.WriteLine(m1.Multi(103, 32));
+
+            m1.Print();
+
+            Console.WriteLine(m1.Sum(1, 100));
+
+
+            Console.WriteLine(MyMath.Abs(52));
+            Console.WriteLine(MyMath.Abs(273));
+            Console.WriteLine(MyMath.Abs(-453));
+
+
+
+        }
+
+        class MyMath
+        {
+            public static int Abs(int input)
+            {
+                return (input < 0) ? -input : input;
+                
+                /*
+                if(input < 0)
+                {
+                    return -input;
+                } 
+                else
+                {
+                    return input;
+                }
+                */
+            }
+        }
+
+        class MultiClass
+        {
+            public int Multi(int x, int y)
+            {
+                return x * y;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("Print() 메서드 호출!");
+            }
+
+            public int Sum(int min, int max)
+            {
+                int output = 0;
+                for (int i = min; i <= max; i++)
+                {
+                    output += i;
+                }
+                return output;
+            }
         }
     }
 }
