c3ceaecbdec4ea3db8fb2586362f43792f6e0fb9
yoenjii310 <yoenjii310@gmail.com>
2021-08-23 14:59:24 +0900

    동물, 강아지, 고양이 클래스 생성

--- CSBasic6/Program.cs
+++ CSBasic6/Program.cs
@@ -6,6 +6,28 @@ using System.Threading.Tasks;
 
 namespace CSBasic6
 {
+    class Animal
+    {
+        public int Age { get; set; }
+        public Animal() { this.Age = 0; }
+
+        public void Eat() { Console.WriteLine("냠냠 먹습니다."); }
+        public void Sleep() { Console.WriteLine("쿨쿨 잠을 잡니다."); }
+    }
+
+    class Dog : Animal
+    {
+        public string Color { get; set; }
+        public void Bark() { Console.WriteLine("왈왈 짓습니다."); }
+    }
+
+    class Cat : Animal
+    {
+        public void Meow() { Console.WriteLine("냥냥 웁니다."); }
+    }
+
+
+
     class Program
     {
         static void Main(string[] args)
