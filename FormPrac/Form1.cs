9d3eceb12f1e6f38242f8597832866629544ce33
yoenjii310 <yoenjii310@gmail.com>
2021-08-25 10:07:08 +0900

    enum 정의하기

--- FormPrac/Form1.cs
+++ FormPrac/Form1.cs
@@ -12,6 +12,11 @@ namespace FormPrac
 {
     public partial class Form1 : Form
     {
+        enum OrderState { Ordered, Paymented, Prepared, Sended };
+        enum Course {  NewMediaSoftware, NewmediaWebsolution, NewmediaDesign};
+        enum CFood { 짜장면, 짬뽕, 볶음밥 };
+
+
         public Form1()
         {
             InitializeComponent();
