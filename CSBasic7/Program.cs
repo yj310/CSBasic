d7190416b8312dd6d09433b2123d961731e7dfe0
yoenjii310 <yoenjii310@gmail.com>
2021-08-30 14:46:43 +0900

    Generic 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,10 +6,28 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
+    class Wanted<T>
+        // where T : Student
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+
+    class Student
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("스트링");
+            Wanted<int> wantedInt = new Wanted<int>(3);
+            Wanted<Student> wanteStudent = new Wanted<Student>(new Student());
         }
     }
 }
