56194b0731c95e19971511351a6bfa5a4217a6a0
yoenjii310 <yoenjii310@gmail.com>
2021-08-30 15:45:26 +0900

    구조체와 구조체 생성자, 객체와의 차이 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -47,6 +47,41 @@ namespace CSBasic7
 
     class Program
     {
+        class PointClass
+        {
+            public int x;
+            public int y;
+
+            public PointClass(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+        struct Point
+        {
+            public int x;
+            public int y;
+            public string testA;
+            // public string textB = "init";
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = "";
+            }
+
+            public Point(int x, int y, string a)
+            {
+                this.x = x;
+                this.y = y;
+                this.testA = a;
+            }
+        }
+
+
+
         static void NextPosition(int x, int y, int vx, int vy, out int rx, out int ry)
         {
             rx = x + vx;
@@ -58,6 +93,36 @@ namespace CSBasic7
 
         static void Main(string[] args)
         {
+            PointClass pcA = new PointClass(10, 20);
+            PointClass pcB = pcA;
+            pcB.x = 100;
+            pcB.y = 200;
+            Console.WriteLine(pcA.x + ", " + pcA.y);
+            Console.WriteLine(pcB.x + ", " + pcB.y);
+            Console.WriteLine();
+
+
+            Point pA = new Point(10, 20);
+            Point pB = pA;
+            pB.x = 100;
+            pB.y = 200;
+            Console.WriteLine(pA.x + ", " + pA.y);
+            Console.WriteLine(pB.x + ", " + pB.y);
+            Console.WriteLine();
+
+
+            Point point = new Point(10, 10);
+            Point point2 = new Point();
+            // point.x = 10;
+            // point.y = 10;
+            Console.WriteLine(point.x);
+            Console.WriteLine(point.y);
+            Console.WriteLine(point2.x);
+            Console.WriteLine(point2.y);
+            Console.WriteLine();
+
+
+
             int x = 0;
             int y = 0;
             int vx = 100;
@@ -69,7 +134,7 @@ namespace CSBasic7
 
 
 
-            Console.WriteLine("숫자 입력: ");
+            Console.Write("숫자 입력: ");
             int output = int.MinValue;
             bool result = int.TryParse(Console.ReadLine(), out output);
 
@@ -81,6 +146,7 @@ namespace CSBasic7
             {
                 Console.WriteLine("숫자를 입력해주세요!" + output);
             }
+            Console.WriteLine();
 
 
 
@@ -88,6 +154,7 @@ namespace CSBasic7
             Products p = new Products();
             Console.WriteLine(p[2]);
             Console.WriteLine((new Products())[2]);
+            Console.WriteLine();
 
 
 
